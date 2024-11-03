e8da4a6ac1c4c3fb3402eade12cc523931a25b76
Deeja-ish <khadijaismail696@gmail.com>
2024-11-02 20:42:33 -1200

    complete

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# Ask user input
+name = input('Please enter your name:  ')
+
+age = input('Please enter your age:   ')
+
+location = input ('Please enter your location:  ')
+
+# print out a personalise statement
+print(f"Hello {name}, Are you {age} years old and live in {location}.")
+
